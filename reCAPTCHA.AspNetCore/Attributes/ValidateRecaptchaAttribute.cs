211970ef76e81198c9a8be27de78cf145233867a
Chuong Nguyen <chuong.nguyen@logixtek.com>
2021-03-27 19:41:14 +0700

    Support to bind attribute

--- reCAPTCHA.AspNetCore/Attributes/ValidateRecaptchaAttribute.cs
+++ reCAPTCHA.AspNetCore/Attributes/ValidateRecaptchaAttribute.cs
@@ -14,6 +14,7 @@ namespace reCAPTCHA.AspNetCore.Attributes
         public bool IsReusable => true;
         private readonly double _minimumScore;
         private readonly string _errorMessage;
+        private readonly string _errorProperty;
 
 
         /// <summary>
@@ -21,16 +22,19 @@ namespace reCAPTCHA.AspNetCore.Attributes
         /// </summary>
         /// <param name="score">The minimum score you wish to be acceptable for a success.</param>
         /// <param name="errorMessage">Error message you want added to validation model.</param>
-        public ValidateRecaptchaAttribute(double score = 0, string errorMessage = "There was an error validating the google recaptcha response. Please try again, or contact the site owner.")
+        public ValidateRecaptchaAttribute(double score = 0,
+            string errorMessage = "There was an error validating the google recaptcha response. Please try again, or contact the site owner.",
+            string mapErrorToProperty = "")
         {
             _minimumScore = score;
             _errorMessage = errorMessage;
+            _errorProperty = mapErrorToProperty;
         }
 
         public IFilterMetadata CreateInstance(IServiceProvider services)
         {
             var recaptcha = services.GetService<IRecaptchaService>();
-            return new ValidateRecaptchaFilter(recaptcha, _minimumScore, _errorMessage);
+            return new ValidateRecaptchaFilter(recaptcha, _minimumScore, _errorMessage, _errorProperty);
         }
     }
 }
